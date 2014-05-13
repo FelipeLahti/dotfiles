2ccb5108db6f5f15df5ada626053bade329b1857
Felipe Lahti <felipe.lahti@gmail.com>
2014-05-13 16:30:42 -0300

    Add directory aliases

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,18 @@
+#Dotfiles
+alias dot='cd ~/.dotfiles'
+
 #Git
 alias g='git'
 alias gs='git status'
 alias gc='git commit'
 alias ga='git add'
 alias gl='git log'
+alias gd='git diff'
+
+#Directories
+alias ..="cd .."
+alias ..2="cd ../.."
+alias ..3="cd ../../.."
+alias ..4="cd ../../../.."
+alias ..5="cd ../../../../.."
+
